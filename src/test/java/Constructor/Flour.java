f4a9f6519772236055d4584b0827a7ad81863e24
Elif <elifnurbirer1994@gmail.com>
2023-01-14 16:27:23 -0800

    first commit

---
+++ src/test/java/Constructor/Flour.java
@@ -0,0 +1,16 @@
+package Constructor;
+
+public class Flour {
+    public Flour (){
+        System.out.println("Bread should contain flour");
+    }
+
+    public Flour (String color){
+        System.out.println("Flour color " + color);
+
+    }
+
+    public Flour (int weight) {
+        System.out.println("Flour weight " + weight);
+    }
+}
